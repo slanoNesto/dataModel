77831fcb99681ed7612cc5c9236722a80b3a6db4
Milos <milos.milutinovic@htec.rs>
2015-08-05 18:53:09 +0200

    added article files, cleared css

---
+++ app/components/articles/articles.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('dataModelApp')
+
+    .controller('articlesController', [function () {
+
+        
+
+    }]);
---
+++ app/components/articles/articles.factory.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('dataModelApp')
+
+    .factory('articlesFactory', [function () {
+
+        return {
+
+        };
+
+
+
+    }]);
